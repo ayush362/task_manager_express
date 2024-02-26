4b7738e9f0480fa0a2e5018e908905b36d601182
Ayush khatri <ayushkhatri362@gmail.com>
2024-02-26 19:45:55 +0530

    Created the methods of get post put delete using routes and controllers

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = 3000;
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("task manager");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}....`);
+});
