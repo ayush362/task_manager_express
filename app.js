d6ae659087b913ab3424866bbf88afa77154c6c5
Ayush khatri <ayushkhatri362@gmail.com>
2024-03-02 13:39:45 +0530

    completed the setup of all the controllers

--- app.js
+++ app.js
@@ -6,24 +6,20 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
-
-// routes
-app.get("/hello", (req, res) => {
-  res.send("task manager");
-});
 
 app.use("/api/v1/tasks", tasks);
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}....`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}....`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
