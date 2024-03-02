eb4a1b5ed30960a9f90a900a75460095a7e6b198
Ayush khatri <ayushkhatri362@gmail.com>
2024-03-02 17:46:10 +0530

    Added some middleware and here completed the projects

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
-
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleWare = require("./middleware/error-handler");
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleWare);
 
 const start = async () => {
     try {
